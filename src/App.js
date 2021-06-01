94e4d1ac7eadabd828c11d94551097b9ba437da7
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-01 19:21:35 +0900

    fix:: Board Component 분리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,96 @@
+import React, { useState } from "react";
 import Board from "./Components/Board";
 
 const App = () => {
-	return <Board />;
+	const [isPlayer, setIsPlayer] = useState(true);
+	const [stepNumber, setStepNumber] = useState(0);
+	const [history, setHistory] = useState([
+		{
+			squares: Array(9).fill(null),
+		},
+	]);
+
+	const handleClick = (i) => {
+		const nowHistory = history.slice(0, stepNumber + 1);
+		const current = nowHistory[nowHistory.length - 1];
+		const squares = current.squares.slice();
+
+		// 이미 선택된 요소는 return
+		if (squares[i] || calculateWinner(squares)) return;
+
+		squares[i] = isPlayer ? "X" : "O";
+
+		setHistory(
+			nowHistory.concat([
+				{
+					squares,
+				},
+			])
+		);
+
+		setIsPlayer(!isPlayer);
+		setStepNumber(history.length);
+	};
+
+	const moves = history.map((step, move) => {
+		const desc = move ? "Go to move #" + move : "Go to game start";
+
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{desc}</button>
+			</li>
+		);
+	});
+
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		setIsPlayer(step % 2 === 0);
+	};
+
+	// =======
+	const winner = calculateWinner(history[history.length - 1].squares);
+	const player = isPlayer ? "X" : "O";
+	let status;
+
+	winner
+		? (status = "Winner : " + winner)
+		: (status = `Next Player : ${player}`);
+
+	return (
+		<div className='game-tictactoe'>
+			<div className='status'>{status}</div>
+			<Board board={history[history.length - 1]} onClick={handleClick} />
+			<div className='record-game'>{moves}</div>
+		</div>
+	);
 };
 
 export default App;
+
+const calculateWinner = (squares) => {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let line of lines) {
+		const [a, b, c] = line;
+
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			// 승자 찾으면, 해당 플레이어 반환
+			return squares[a];
+		}
+	}
+	// 승자가 없으면 null
+	return null;
+};
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,69 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Square from "./Square";
 
-const Board = () => {
-	const [isPlayer, setIsPlayer] = useState(true);
-	const [stepNumber, setStepNumber] = useState(0);
-	const [history, setHistory] = useState([
-		{
-			squares: Array(9).fill(null),
-		},
-	]);
-
+const Board = (props) => {
 	const renderSquares = (i) => {
 		return (
 			<Square
-				value={history[stepNumber].squares[i]}
-				onClick={() => handleClick(i)}
+				value={props.board.squares[i]}
+				onClick={() => props.onClick(i)}
 			/>
 		);
 	};
 
-	const handleClick = (i) => {
-		const nowHistory = history.slice(0, stepNumber + 1);
-		const current = nowHistory[nowHistory.length - 1];
-		const squares = current.squares.slice();
-
-		// 이미 선택된 요소는 return
-		if (squares[i] || calculateWinner(squares)) return;
-
-		squares[i] = isPlayer ? "X" : "O";
-		setHistory(
-			nowHistory.concat([
-				{
-					squares,
-				},
-			])
-		);
-		setIsPlayer(!isPlayer);
-		setStepNumber(history.length);
-		console.log(stepNumber);
-	};
-
-	const moves = history.map((step, move) => {
-		const desc = move ? "Go to move #" + move : "Go to game start";
-
-		return (
-			<li key={move}>
-				<button onClick={() => jumpTo(move)}>{desc}</button>
-			</li>
-		);
-	});
-
-	const jumpTo = (step) => {
-		setStepNumber(step);
-		setIsPlayer(step % 2 === 0);
-	};
-
-	// =======================================
-
-	const winner = calculateWinner(history[history.length - 1].squares);
-	const player = isPlayer ? "X" : "O";
-	let status;
-
-	winner
-		? (status = "Winner : " + winner)
-		: (status = `Next Player : ${player}`);
 	return (
 		<div className='board'>
 			<div className='board-row'>
@@ -81,41 +28,8 @@ const Board = () => {
 				{renderSquares(7)}
 				{renderSquares(8)}
 			</div>
-
-			<div className='game-info'>
-				<div>{status}</div>
-				<ol>{moves}</ol>
-			</div>
 		</div>
 	);
 };
 
 export default Board;
-
-const calculateWinner = (squares) => {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-
-	for (let line of lines) {
-		const [a, b, c] = line;
-
-		if (
-			squares[a] &&
-			squares[a] === squares[b] &&
-			squares[a] === squares[c]
-		) {
-			// 승자 찾으면, 해당 플레이어 반환
-			return squares[a];
-		}
-	}
-	// 승자가 없으면 null
-	return null;
-};

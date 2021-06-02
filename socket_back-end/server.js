b2a3394a9d66df464d7bc1a55f2f7d7923ceab78
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-02 11:18:03 +0900

    fix : CORS 문제 해결

--- socket_back-end/server.js
+++ socket_back-end/server.js
@@ -1,14 +1,25 @@
-import dotenv from "dotenv";
-import express from "express";
-import * as socketIO from "socket.io";
-import http from "http";
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
+const PORT = process.env.PORT;
 
-const app = express();
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
-const io = new socketIO.Server(server);
+var app = require("http").createServer(function (req, res) {
+	// Set CORS headers
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:4004/");
+	res.setHeader("Access-Control-Request-Method", "*");
+	res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+	res.setHeader("Access-Control-Allow-Headers", "*");
+	if (req.method === "OPTIONS" || req.method === "GET") {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
+});
+
+var io = require("socket.io")(app);
 
 io.on("connection", (socket) => {
 	socket.on("join", ({ roomName: room, userName: user }) => {
@@ -26,4 +37,4 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(PORT, () => console.log(`✅ Listening on POPRT : ${PORT} `));
+app.listen(PORT);

4ca4e7dd2916dbfb88b185b28ddf8d02af4057d5
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-02 09:10:19 +0900

    feat : 서버 socket connect 구현

--- socket_back-end/server.js
+++ socket_back-end/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import socketIO from "socket.io";
 import http from "http";
 
 dotenv.config();
@@ -7,5 +8,22 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+	socket.on("join", ({ roomName: room, userName: user }) => {
+		socket.join(room);
+		io.to(room).emmit("onConnect", `${user} 님이 입장했습니다.`);
+
+		socket.on("onSend", (messageItem) => {
+			io.to(room).emit("onReceive", messageItem);
+		});
+
+		socket.on("disconnect", () => {
+			socket.leave(room);
+			io.to(room).emit("onDisconnect", `${user} 님이 퇴장하셨습니다.`);
+		});
+	});
+});
 
 server.listen(PORT, () => console.log(`✅ Listening on POPRT : ${PORT} `));

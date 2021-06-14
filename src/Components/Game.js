376c4e448c840b4fc4f1f3c9ea808d56268dd937
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-14 21:32:39 +0900

    feat:: 자기 차례에만 게임 플레이 가능

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,10 +6,13 @@ const Game = (props) => {
 	const [board, setBoard] = useState(Array(9).fill(null));
 	const [tempBoard, setTempBoard] = useState(Array(9).fill(null));
 	const [tempSquares, setTempSquares] = useState(Array(9).fill(null));
+	const [isYourTurn, setIsYourTurn] = useState();
 
 	useEffect(() => {
-		props.socket.on("sendSquare", ({ currentBoard }) => {
+		props.socket.on("sendSquare", ({ currentBoard, isGameTurn }) => {
 			setTempBoard(currentBoard);
+			console.log(isGameTurn);
+			setIsYourTurn(isGameTurn);
 		});
 	}, []);
 
@@ -24,21 +27,27 @@ const Game = (props) => {
 
 	const handleClick = (i) => {
 		const squares = board.slice();
+		console.log(
+			`userTurn : ${props.userTurn} , isYourTurn in handler : ${isYourTurn}`
+		);
 
+		if (squares[i] !== null || isYourTurn !== props.userTurn) return;
 		squares[i] = props.userTurn ? "O" : "X";
 
 		setTempSquares(squares);
 	};
 	// =======
-
-	let status;
 	return (
 		<div className='game-tictactoe'>
 			<div>
 				<h3>방번호 : {props.state.roomName}</h3>
 				<h3>유저 이름 : {props.state.userName}</h3>
 			</div>
-			<div className='status'>{status}</div>
+			<div className='status'>
+				{props.userTurn === isYourTurn
+					? "당신 차례입니다."
+					: "상대방 차례입니다."}
+			</div>
 			<Board squares={board} onClick={handleClick} />
 			<Chat state={props.state} socket={props.socket} />
 		</div>

dd904d58d265b07b5ad7520681b83ad8cde38e2f
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-15 11:35:00 +0900

    WaitingRoom CSS 적용

--- src/Components/WaitingRoom.js
+++ src/Components/WaitingRoom.js
@@ -1,6 +1,6 @@
 import React from "react";
 import dotenv from "dotenv";
-
+import "./WaitingRoom.css";
 dotenv.config();
 
 const WaitingRoom = (props) => {
@@ -25,12 +25,26 @@ const WaitingRoom = (props) => {
 	};
 
 	return (
-		<div>
-			<label htmlFor='user-name'>닉네임</label>
-			<input className='user-name' onChange={handleUserName}></input>
-			<label htmlFor='room-name'>방번호</label>
-			<input className='room-name' onChange={handleRoomName}></input>
-			<button onClick={handleJoinRoom}>전송</button>
+		<div className='enter-game'>
+			<h1 className='enter-game-title'>틱택토 🎮</h1>
+			<h3 className='enter-game-desc'>누구나 쉽게 즐길 수 있는 게임</h3>
+			<div className='enter-game-form'>
+				<label htmlFor='user-name'>닉네임</label>
+				<input
+					className='user-name'
+					onChange={handleUserName}
+					placeholder='닉네임을 입력하세요'
+				></input>
+				<label htmlFor='room-name'>방번호</label>
+				<input
+					className='room-name'
+					onChange={handleRoomName}
+					placeholder='입장하실 방 번호를 입력하세요'
+				></input>
+				<button className='enter-room' onClick={handleJoinRoom}>
+					시작하기 !
+				</button>
+			</div>
 		</div>
 	);
 };
